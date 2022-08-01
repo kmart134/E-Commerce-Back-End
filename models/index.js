21acd80405753fc6471f38b57aa7803902d6d27d
kmart134 <ksmgps@hotmail.com>
2022-08-01 07:36:54 -0400

    models.index.js updated

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+//dont know how to fill
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+//dont know how to fill
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    //unique???
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product,{
+  through: {
+    model: ProductTag,
+    //unique???
+  }
+});
+
 
 module.exports = {
   Product,
